9e28b4b3965908b6f075a518ad083b6761733187
celine-y <clubs43v@gmail.com>
2017-02-20 21:36:02 -0500

    fix: active home tab w hen relaoding on home

--- js/nav-slide.js
+++ js/nav-slide.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Changing nav when scrolling
     $(document).on('scroll', onScroll);
 
-    function onScroll(e){
+    function onScroll(e) {
         var scrollPos = $(document).scrollTop();
         $('#nav a').each(function () {
             var currLink = $(this);
@@ -11,31 +11,31 @@ $(document).ready(function() {
                 $('#nav ul li a').removeClass("active");
                 currLink.addClass("active");
             }
-            else{
+            else {
                 currLink.removeClass("active");
             }
         });
     }
 
 
-// <!----- JQUERY FOR SLIDING NAVIGATION ---> 
-  $('a[href*=#]').each(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
+    // <!----- JQUERY FOR SLIDING NAVIGATION ---> 
+    $('a[href*=#]').each(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname
+            && this.hash.replace(/#/, '')) {
+            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+            if ($target) {
+                var targetOffset = $target.offset().top;
 
-// <!----- JQUERY CLICK FUNCTION REMOVE AND ADD CLASS "ACTIVE" + SCROLL TO THE #DIV--->   
-         $(this).click(function() {
-            $("#nav li a").removeClass("active");
-            $(this).addClass('active');
-           $('html, body').animate({scrollTop: targetOffset}, 1000);
-           return false;
-         });
-      }
-    }
-  });
+                // <!----- JQUERY CLICK FUNCTION REMOVE AND ADD CLASS "ACTIVE" + SCROLL TO THE #DIV--->   
+                $(this).click(function () {
+                    $("#nav li a").removeClass("active");
+                    $(this).addClass('active');
+                    $('html, body').animate({ scrollTop: targetOffset }, 1000);
+                    return false;
+                });
+            }
+        }
+    });
 });
