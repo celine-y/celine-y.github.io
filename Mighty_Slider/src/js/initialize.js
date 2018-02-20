7fd5d39d4703be57cfec68720075d69be9b742a3
celine-y <clubs43v@gmail.com>
2018-02-19 21:23:43 -0500

    Revert "Updating website with "sunset" design"

---
+++ Mighty_Slider/src/js/initialize.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+    // $('.modern').each(function(){
+    //     console.log("enter");
+    //     $('.frame').mightySlider({
+    //         autoScale: 1,
+    //     });
+    // }); 
+        
+    // });
+    // jQuery('.frame').each(function(){
+    //     $('.frame').mightySlider();
+    // });
+
+  });
